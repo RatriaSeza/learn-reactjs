0477eab38c30cd16a605ea8e22aae7731c9faf2d
RatriaSeza <satriareza2002@gmail.com>
2023-04-13 20:29:23 +0700

    01-setup and introduction

---
+++ src/components/introduction.js
@@ -0,0 +1,12 @@
+import "../App.css";
+
+function Introduction( props ) {
+	return (
+		<div>
+			<h1>INTRODUCTION</h1>
+			<p>Hi, my name is {props.name}</p>
+		</div>
+	);
+}
+
+export default Introduction;
